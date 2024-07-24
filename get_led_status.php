b30f1afdabec8ac425e849eab0db2c9be184a236
Horusyst <abraham.garces@hotmail.com>
2024-07-24 00:27:03 -0500

    Create get_led_status.php

---
+++ get_led_status.php
@@ -0,0 +1,22 @@
+<?php
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "led_control";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(['success' => false, 'message' => 'Connection failed']));
+}
+
+$result = $conn->query("SELECT led_id, status FROM led_status");
+$leds = [];
+while ($row = $result->fetch_assoc()) {
+    $leds[] = $row;
+}
+echo json_encode($leds);
+
+$conn->close();
+?>
