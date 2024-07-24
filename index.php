8bcd051e07230cde464e0113259f9140e903f2c1
Horusyst <abraham.garces@hotmail.com>
2024-07-23 23:35:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,143 +1,55 @@
 <?php
-
+// Conexión a la base de datos
 $servername = "localhost";
-$dBUsername = "Horusyst";
-$dBPassword = "Braham1243*";
-$dBName = "https://horusyst.github.io/luz/";
+$username = "Horusyst";
+$password = "Braham1243*";
+$dbname = "BasededatosLED";
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if (!$conn) {
-	die("Connection failed: ".mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-
-if (isset($_POST['toggle_LED'])) {
-	$sql = "SELECT * FROM Led_status;";
-	$result   = mysqli_query($conn, $sql);
-	$row  = mysqli_fetch_assoc($result);
-	
-	if($row['status'] == 0){
-		$update = mysqli_query($conn, "UPDATE Led_status SET status = 1 WHERE id = 1;");		
-	}		
-	else{
-		$update = mysqli_query($conn, "UPDATE Led_status SET status = 0 WHERE id = 1;");		
-	}
+// Manejar cambios de estado de los LEDs
+if (isset($_GET['toggle_led'])) {
+    $led_id = intval($_GET['toggle_led']);
+    $result = $conn->query("SELECT status FROM led_status WHERE led_id = $led_id");
+    $row = $result->fetch_assoc();
+    $new_status = $row['status'] ? 0 : 1;
+    $conn->query("UPDATE led_status SET status = $new_status WHERE led_id = $led_id");
 }
 
-
-
-$sql = "SELECT * FROM Led_status;";
-$result   = mysqli_query($conn, $sql);
-$row  = mysqli_fetch_assoc($result);	
-
+// Obtener el estado actual de los LEDs
+$result = $conn->query("SELECT led_id, status FROM led_status");
+$leds = [];
+while ($row = $result->fetch_assoc()) {
+    $leds[] = $row;
+}
+$conn->close();
 ?>
 
-<style>
-	.wrapper{
-		width: 100%;
-		padding-top: 50px;
-	}
-	.col_3{
-		width: 33.3333333%;
-		float: left;
-		min-height: 1px;
-	}
-	#submit_button{
-		background-color: #2bbaff; 
-		color: #FFF; 
-		font-weight: bold; 
-		font-size: 40; 
-		border-radius: 15px;
-    	text-align: center;
-	}
-	.led_img{
-		height: 400px;		
-		width: 100%;
-		object-fit: cover;
-		object-position: center;
-	}
-	
-	@media only screen and (max-width: 600px) {
-		.col_3 {
-			width: 100%;
-		}
-		.wrapper{
-			width: 100%;
-			padding-top: 5px;
-		}
-		.led_img{
-			height: 300px;		
-			width: 80%;
-			margin-right: 10%;
-			margin-left: 10%;
-			object-fit: cover;
-			object-position: center;
-		}
-	}
-
-</style>
-
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js" type="text/javascript"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control de LEDs</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-	<div class="wrapper" id="refresh">
-		<div class="col_3">
-		</div>
-
-		<div class="col_3" >
-			
-			<?php echo '<h1 style="text-align: center;">The status of the LED is: '.$row['status'].'</h1>';?>
-			
-			<div class="col_3">
-			</div>
-			
-			<div class="col_3" style="text-align: center;">
-			<form action="index.php" method="post" id="LED" enctype="multipart/form-data">			
-				<input id="submit_button" type="submit" name="toggle_LED" value="Toggle LED" />
-			</form>
-				
-			<script type="text/javascript">
-			$(document).ready (function () {
-				var updater = setTimeout (function () {
-					$('div#refresh').load ('index.php', 'update=true');
-				}, 1000);
-			});
-			</script>
-			<br>
-			<br>
-			<?php
-				if($row['status'] == 0){?>
-				<div class="led_img">
-					<img id="contest_img" src="led_off.png" width="100%" height="100%">
-				</div>
-			<?php	
-				}
-				else{ ?>
-				<div class="led_img">
-					<img id="contest_img" src="led_on.png" width="100%" height="100%">
-				</div>
-			<?php
-				}
-			?>
-			
-				
-				
-				
-			</div>
-				
-			<div class="col_3">
-			</div>
-		</div>
-
-		<div class="col_3">
-		</div>
-	</div>
+    <h1>Control de LEDs</h1>
+    <div class="led-container">
+        <?php foreach ($leds as $led): ?>
+            <div class="led-control">
+                <img src="images/bulb.png" alt="Bombillo">
+                <button
+                    class="led-button <?php echo $led['status'] ? 'led-on' : 'led-off'; ?>"
+                    onclick="window.location.href='?toggle_led=<?php echo $led['led_id']; ?>'">
+                    LED <?php echo $led['led_id']; ?>
+                </button>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
-</html>
-
 </html>
