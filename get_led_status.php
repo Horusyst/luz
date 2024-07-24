32367160e50932d94d21f58c6771851d0c114ffd
Horusyst <abraham.garces@hotmail.com>
2024-07-24 00:50:54 -0500

    Update get_led_status.php

--- get_led_status.php
+++ get_led_status.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "Horusyst";
 $password = "Braham1243*";
-$dbname = "BasesdedatosLED.sql";
+$dbname = "BasededatosLED.sql";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
