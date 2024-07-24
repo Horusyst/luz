f41b954c01c0659c2c6515f383e9ae232a051c75
Horusyst <abraham.garces@hotmail.com>
2024-07-24 03:26:25 -0500

    Update get_led_status.php

--- get_led_status.php
+++ get_led_status.php
@@ -5,7 +5,7 @@ $username = "mysql.infoschema";
 $password = "Abraham1243";
 $dbname = "led_control.sql";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect( $servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die(json_encode(['success' => false, 'message' => 'Connection failed']));
