353436c1516bc035ced6704cebafceeea316b7af
Horusyst <abraham.garces@hotmail.com>
2024-07-23 23:28:32 -0500

    Update esp32_update.php

--- esp32_update.php
+++ esp32_update.php
@@ -9,32 +9,32 @@ if (!$conn) {
 }
 
 //Read the database
-if (isset($_POST['check_LED_status'])) {
+if (isset($_POST['check_Led_status'])) {
 	$led_id = $_POST['check_LED_status'];	
-	$sql = "SELECT * FROM LED_status WHERE id = '$led_id';";
+	$sql = "SELECT * FROM Led_status WHERE id = '$led_id';";
 	$result   = mysqli_query($conn, $sql);
 	$row  = mysqli_fetch_assoc($result);
 	if($row['status'] == 0){
-		echo "LED_is_off";
+		echo "Led_is_off";
 	}
 	else{
-		echo "LED_is_on";
+		echo "Led_is_on";
 	}	
 }	
 
 //Update the database
 if (isset($_POST['toggle_LED'])) {
 	$led_id = $_POST['toggle_LED'];	
-	$sql = "SELECT * FROM LED_status WHERE id = '$led_id';";
+	$sql = "SELECT * FROM Led_status WHERE id = '$led_id';";
 	$result   = mysqli_query($conn, $sql);
 	$row  = mysqli_fetch_assoc($result);
 	if($row['status'] == 0){
-		$update = mysqli_query($conn, "UPDATE LED_status SET status = 1 WHERE id = 1;");
-		echo "LED_is_on";
+		$update = mysqli_query($conn, "UPDATE Led_status SET status = 1 WHERE id = 1;");
+		echo "Led_is_on";
 	}
 	else{
-		$update = mysqli_query($conn, "UPDATE LED_status SET status = 0 WHERE id = 1;");
-		echo "LED_is_off";
+		$update = mysqli_query($conn, "UPDATE Led_status SET status = 0 WHERE id = 1;");
+		echo "Led_is_off";
 	}	
 }	
 ?>
